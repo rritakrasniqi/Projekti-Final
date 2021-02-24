74426e18ad2a03551ce40d3b491a803a9285458d
EneaBrovina <75734637+EneaBrovina@users.noreply.github.com>
2021-02-24 22:37:50 +0100

    About
    About pages

---
+++ Projekti-Web/pages/about.php
@@ -0,0 +1,121 @@
+<!DOCTYPE html>
+<html lang="">
+
+<head>
+<title>Login | Page | Flower shop </title>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
+<link href="../layout/styles/layout.css" rel="stylesheet" type="text/css" media="all">
+</head>
+<body id="top">
+
+<div class="wrapper row0">
+  <div id="topbar" class="hoc clear"> 
+    
+    <div class="fl_left">
+      <ul class="nospace">
+        <li><i class="fa fa-phone"></i> +00 (123) 456 7890</li>
+        <li><i class="fa fa-envelope-o"></i> info@FlowerShop.com</li>
+      </ul>
+    </div>
+    <div class="fl_right">
+      <ul class="nospace">
+        <li><a href="../index.php"><i class="fa fa-lg fa-home"></i></a></li>
+        <li><a href="about.php">About</a></li>
+        <li><a href="contact.php">Contact</a></li>
+		<li><a href="login.php">Login</a></li>
+        <li><a href="register.php">Register</a></li>
+        <li><a href="Upload_pdf.php">Upload</a></li>
+		<li><i class="fa fa-shopping-cart"></i>shopping cart</li>
+      </ul>
+    </div>
+   
+  </div>
+</div>
+
+<div class="wrapper row1">
+  <header id="header" class="hoc clear"> 
+
+    <div id="logo" class="fl_left">
+      <h1><a href="../index.php">Flower Shop</a></h1>
+    </div>
+	
+  </header>
+</div>
+
+
+<div class="wrapper" style="background:url(../images/demo/work.png);">
+  <main class="container hoc clear"> 
+     
+     <div class="wrapper row3">
+  <main class="hoc container clear"> 
+    <div class="sectiontitle"><h6> About us </h6>
+      <h3 class="heading">Serenata Flowers</h3>
+      <p>Fresh Flowers From Prishtina</p>
+    </div>
+    <p class="btmspace-50 justified"> <p> 
+
+‘Prishtina Flower Shop’ knows that clients’ fulfillment is of real noteworthiness to any business. Indeed disappointed clients will in general offer their sentiment more than the satisfied ones without being inquired. This happens on the grounds that they’re irritated and exasperated. That is the reason client benefit must be the essential center when you begin an organization. The accentuation must be on you, generally.
+
+Yet, how might we achieve an abnormal state of consumer loyalty? By giving brilliant flower conveyance administrations, obviously. Furthermore, our flower game plans are constantly made after we get a request and not previously. Accordingly the flower bunch remains fresher for longer after it’s been conveyed to the location we’re given. And keeping in mind that being transported, the flowers are in a sheltered place and kept at a cool temperature in our vans.
+
+We trust that we’ll continue attracting new clients to us by offering assorted variety, which implies an extraordinary assortment of flowers, inflatables and champagne. Our assorted variety likewise incorporates diverse sorts of flower courses of action for a wide range of events. To the extent flowers go, we have something for any taste. Orchids, lilies, sunflowers, roses, tulips thus considerably more can be found on our site and after that conveyed to the doorstep of a companion, relative or a friend or family member of yours.
+
+As it were, we can turn out to be a piece of your unique occasions or improve any home through the flower game plans we make. Also, they are lovely, to say it gently. Be that as it may, our flowers can be far beyond essentially delightful; they can be splendid and brilliant, unobtrusive and fragile, cheering and crucial, quiet and serene, etc. The tremendous adaptability of flower types as well as of feelings they express can’t be denied.
+
+Alongside flowers and inflatables, we additionally have champagne, chocolate, pruned plants, hand-tied bunches and other little shocks that you can uncover by calling us . Our flower arrangers have the right to be perceived.
+
+They are the ones who make the magnificent flower bundles that are so unique and event proper, which definitely demonstrates how inventive their creative energies are. Orchestrating flowers requires ability and inventiveness as well as learning of explicit methods for making bundles and laurels.
+
+We wouldn’t have any desire to miss referencing the real flower conveyance staff. They are the ones that the beneficiaries of our flowers see on their doorsteps. That is the reason they need to look respectable while conveying flowers</p>
+   
+    <ul class="nospace group center">
+      <li class="one_quarter first">
+        <img  src="../images/demo/w3.png" alt="">
+        <p>Our Team</p>
+		
+      </li>
+      <li class="one_quarter">
+        <img src="../images/demo/w.png" alt="">
+        <p>Our Place</p>
+	
+      </li>
+      <li class="one_quarter">
+      <img src="../images/demo/w2.png" alt="">
+       <p>Working</p>
+		
+      </li>
+      <li class="one_quarter">
+        <img src="../images/demo/w4.png" alt="">
+         <p>Our Space</p>
+	
+      </li>
+	  
+    </ul>
+    <div class="clear"></div>
+  </main>
+</div>
+
+  
+    <div class="clear"></div>
+  </main>
+  <div class="wrapper bgded overlay" >
+    <article class="hoc container clear"> 
+         
+        <h6 class="nospace">You can find us online</h6>
+   
+       </article>
+  </div>
+</div>
+
+<div class="wrapper row5">
+  <div id="copyright" class="hoc clear"> 
+   
+    <p class="fl_left">Copyright &copy; 2020 All Rights Reserved </p>
+    <p class="fl_right">Flower Shop Prishtina </p>
+    
+  </div>
+</div>
+
+<script src="../layout/scripts/script.js"></script>
+</html>
